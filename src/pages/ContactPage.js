c40cf0f4b417c16b8a9c5d0e0de0114f9b7ad2c5
Carlos Lizarde <carlos.lizarde@consultant.volvo.com>
2023-08-07 21:05:08 +0200

    failing test fixed

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,6 +1,6 @@
 import React from "react";
-import axios from 'axios';
 import { Container, Row, Col, Form, Button } from "react-bootstrap";
+import axios from 'axios';
 
 const handleSubmit = async (event) => {
   event.preventDefault();
@@ -66,6 +66,7 @@ const ContactPage = () => {
             415 12 Göteborg Sweden <br />
           </p>
           <iframe
+            title="google maps"
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d532.5861848436972!2d12.006003769703737!3d57.727587514444004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x464ff4622d06bab5%3A0x18d255c4cfaf9169!2sGamlestads%20Brygga%2017%2C%20415%2012%20G%C3%B6teborg!5e0!3m2!1sen!2sse!4v1686072737149!5m2!1sen!2sse"
             width="100%"
             height="450"
